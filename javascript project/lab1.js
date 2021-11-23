740ed653131e1bbf36ac7ced6667223201136e9a
Roxxi-Artz <90343355+Roxxi-Artz@users.noreply.github.com>
2021-11-23 13:06:46 -0500

    Add files via upload

---
+++ javascript project/lab1.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+document.write("The number of seconds in an hour is " + secondsInAnHour);
+document.write("<br>");
+
+var secondsInADay = secondsInAnHour * hoursInADay;
+document.write("The number of seconds in a day are " + secondsInADay);
+document.write("<br>");
+
+var secondsInAYear = secondsInADay * daysInAYear;
+document.write("The number of seconds in a year are " + secondsInAYear);
+document.write("<br>");
+document.write("<br>");
+document.write("Created by Roxanne Poynter")
